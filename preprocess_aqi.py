5507d00d2f3cf32080b1199988e4d9dec9a85d9e
Sampo Savolainen <savolainen.sampo@gmail.com>
2018-10-10 15:21:59 +0300

    Modify AQI preprocessing code to only add coordinates to AQI data frame

--- preprocess_aqi.py
+++ preprocess_aqi.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def preprocess_site_list(site_file):
     # Make sure that state, county, and site codes are read as strings
     # instead of numbers
@@ -12,7 +13,9 @@ def preprocess_site_list(site_file):
         'Land Use', 'Location Setting', 'Met Site State Code',
         'Met Site County Code', 'Met Site Site Number', 'Met Site Type',
         'Met Site Distance', 'Met Site Direction', 'GMT Offset',
-        'Owning Agency', 'Local Site Name'
+        'Owning Agency', 'Local Site Name', "Address", "Zip Code",
+        "State Name", "County Name", "City Name", "CBSA Name", "Tribe Name",
+        "Extraction Date"
     ]
 
     sites = sites.drop(columns=cols_to_drop)
@@ -33,20 +36,46 @@ def preprocess_site_list(site_file):
         'County Code'] + '-' + sites['Site Number']
     sites = sites.set_index('site-code')
 
-    # Transform the site listing into a dictionary where keys are site
-    # codes and values are dictionaries of type {column: value}
-    # {site-code1: {column1: value1}, {column2: value2}...}
-    site_list = sites.to_dict('index')
+    # Divide the data into two Series: one with latitude coordinates and one
+    # with longitude coordinates
+    sites_lat = sites['Latitude']
+    sites_lon = sites['Longitude']
 
-    return site_list
+    # Transform the site listings into a dictionary where keys are site
+    # codes and values are coordinates}
+    sites_lat = sites_lat.to_dict()
+    sites_lon = sites_lon.to_dict()
+
+    # Return the two dictionaries with the coordinates
+    return sites_lat, sites_lon
 
 
-def preprocess_aqi(site_list, aqi_file):
+def preprocess_aqi(site_file, aqi_file):
     # Read daily AQI measurements from year XXXX
     aqi = pd.read_csv(aqi_file)
 
+    # Drop unnecessary columns
+    cols_to_drop = [
+        'State Name', 'State Code', 'county Name', 'County Code',
+        'Number of Sites Reporting'
+    ]
+    aqi.drop(columns=cols_to_drop, inplace=True)
+
+    # Get coordinate information for measurement sites
+    lat_coords, lon_coords = preprocess_site_list(site_file)
+
     # Add column containing the site information (including location
     # coordinates) to each AQI measurement
-    aqi['site-information'] = aqi['Defining Site'].map(site_list)
+    aqi['Latitude'] = aqi['Defining Site'].map(lat_coords)
+    aqi['Longitude'] = aqi['Defining Site'].map(lon_coords)
 
     return aqi
+
+
+# Main function for checking that everything works
+def main():
+    aqi = preprocess_aqi('aqs_sites.csv', 'daily_aqi_by_county_2015.csv')
+    print(aqi.head())
+
+if __name__ == '__main__':
+    main()
